6c288f9c789bcdacafa9fedd6757b052e50629eb
be-better-now <huuduy.uwu@gmail.com>
2025-03-28 14:41:55 +0700

    add example about Java class

---
+++ examples/Java_Class/src/Hello.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author huudu
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); //In ra màn hình: Hello World!
+                                            //gõ "sout" + Ctrl Space để in nhanh ra câu lệnh System.out.println("...");
+    }
+}
